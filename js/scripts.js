7753ef7004b386029dfe94809671b71be7438d64
ahmed sanad <ahsanad@gmail.com>
2020-11-13 21:39:02 +0200

    uploading my portfolio

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+/*!
+    * Start Bootstrap - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+    */
+    (function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 75
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  
+// portfolio section
+var $projects = $('.projects');
+
+$projects.isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+});
+
+$('ul.filters > li').on('click', function(e){
+
+    e.preventDefault();
+
+    var filter = $(this).attr('data-filter');
+
+    $('ul.filters > li').removeClass('active');
+    $(this).addClass('active');
+
+    $projects.isotope({filter: filter});
+
+});
+
+$('.card').mouseenter(function(){
+
+    $(this).find('.card-overlay').css({'top': '-100%'});
+    $(this).find('.card-hover').css({'top':'0'});
+
+}).mouseleave(function(){
+
+    $(this).find('.card-overlay').css({'top': '0'});
+    $(this).find('.card-hover').css({'top':'100%'});
+
+});
+
+
+  
+
+})(jQuery); // End of use strict
+
+
